92e3aaf9c1464707b6a83d9a249e55810936140c
Ricardo Ferreira <129761040+ricardojcf9@users.noreply.github.com>
2023-04-03 14:42:06 +0100

    Aula 4

---
+++ Aula4/Parte II/Ex4/100Hz/100hz.c
@@ -0,0 +1,34 @@
+#include <detpic32.h>
+
+void delay(int unsigned ms)
+{
+for ( ; ms > 0; ms--) 
+	{
+resetCoreTimer();
+while (readCoreTimer() < 20000);
+	}
+}
+int main(void)
+{
+unsigned char segment;
+TRISB = TRISB & 0x80FF;			// configure RB8-RB14 as outputs
+TRISD = TRISD & 0xFF9F;			// configure RD5-RD6 as outputs
+LATD = (LATD & 0xFF9F) | 0x40;		// sets RD6 and RD5 to 0 and then sets RD6 to 1
+
+while(1)
+{
+
+LATD = LATD ^ 0x60;
+
+
+segment = 1;
+int i;
+for(i=0; i < 7; i++)
+{
+LATB = (LATB & 0x80FF) | segment << 8;	// send "segment" value to display
+delay(10);				// wait 0.5 second
+segment = segment << 1;
+}
+}
+return 0;
+}
---
+++ Aula4/Parte II/Ex6/dip.c
@@ -0,0 +1,27 @@
+#include <detpic32.h>
+
+void delay(int unsigned ms)
+{
+for ( ; ms > 0; ms--) 
+	{
+resetCoreTimer();
+while (readCoreTimer() < 20000);
+	}
+}
+
+int main(void)
+{
+static const char disp7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+TRISB = TRISB & 0x000F;		// configure RB0 to RB3 as input
+TRISB = TRISB & 0x80FF;		// configure RB8 to RB14 as outputs
+TRISD = TRISD & 0xFF9F;		// configure RD5 to RD6 as outputs
+LATD = (LATD & 0xFF9F) | 0x20;	// Select display low
+
+while(1)
+{
+int dipValue = PORTB & 0x000F; // read dip-switch
+int code = disp7Scodes[dipValue];
+LATB = (LATB & 0x80FF) | code << 8;
+}
+return 0;
+}
