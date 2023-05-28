cade75a0b284516a99b33a51ddb40d3fc92a0375
Ricardo Ferreira <129761040+ricardojcf9@users.noreply.github.com>
2023-05-28 21:45:12 +0100

    28/05 update

--- Aula07/Parte II/Ex1/ex1.c
+++ Aula07/Parte II/Ex1/ex1.c
@@ -1,22 +1,13 @@
 #include <detpic32.h>
 
-					// Interrupt Handler
-
-void _int_(27) isr_adc(void) 		// Replace VECTOR by the A/D vector
-					// number - see "PIC32 family data
-					// sheet" (pages 74-76)
-{
-int aux = ADC1BUF0;
-printInt (aux, 10);			// Read conversion result (ADC1BUF0) and print it
-AD1CON1bits.ASAM = 1;			// Start A/D conversion
-IFS1bits.AD1IF = 0; 			// Reset AD1IF flag
-}
-
 int main(void)
 {
+
+TRISDbits.TRISD11 = 0;
+
 					// Configure all (digital I/O, analog input, A/D module)
 TRISBbits.TRISB4 = 1; 			// RBx digital output disconnected
-AD1PCFGbits.PCFG4= 0; 			// RBx configured as analog input
+AD1PCFGbits.PCFG4 = 0; 			// RBx configured as analog input
 AD1CON1bits.SSRC = 7; 			// Conversion trigger selection bits: in this mode an internal counter ends sampling and 
 					// starts conversion
 AD1CON1bits.CLRASAM = 1; 		// Stop conversions when the 1st A/D converter interrupt is generated. At the same time, hardware
@@ -39,4 +30,18 @@ while(1);				// all activity is done by the ISR
 
 return 0;
 
+}
+
+					// Interrupt Handler
+
+void _int_(27) isr_adc(void) 		// Replace VECTOR by the A/D vector
+					// number - see "PIC32 family data
+					// sheet" (pages 74-76)
+{
+volatile int adc_value;
+LATDbits.LATD11 = 0;			// Reset RD11 (LATD11 = 0)
+adc_value = ADC1BUF0;			// Read ADC1BUF0 value to "adc_value"
+AD1CON1bits.ASAM = 1;			// Start A/D conversion
+LATDbits.LATD11 = 1;			// Set RD11 (LATD11 = 1)
+IFS1bits.AD1IF = 0; 			// Reset AD1IF flag
 }
